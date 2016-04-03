af0bc530524419fcd0b9a5561db393e544e35676
Marinaro94 <marinaromike@gmail.com>
2016-04-03 12:19:27 -0400

    AreaPredictor

--- src/io/evolution/AreaPredictor.java
+++ src/io/evolution/AreaPredictor.java
@@ -1,15 +1,70 @@
 package io.evolution;
+import java.util.ArrayList;
 
-import java.sql.Connection;
+
 
 /**
- * Created by 1994r on 4/3/2016.
+ * Created by michael on 4/3/2016.
  */
 public class AreaPredictor {
-    public AreaPredictor(Connection c, String MMSI, String startDate, String startTime, String endDate, String endTime){
 
+    private float[] initialCoordinates;
+    private float[] primaryBoundry;
+    private ArrayList<float[]> outerBoundryCoordinates;
+
+    private int travelTime;
+    private float vesselSpeed;
+
+
+    public AreaPredictor(int time,float knots) {
+        float travelTime = time;
+        float vesselSpeed = knots;
+        ArrayList<float[]> outerBoundryCoordinates = new ArrayList<float[]>();
     }
-    public boolean execute(){
-        return true;
+
+    private float getHeading(){
+        //Get last known coordinates
+        //Get second-to-last known coordinates
+        //Use math function to determine the degree between the two latitude points
+        //Return heading in degrees
     }
+
+
+    private int getDistance(int time,float knots){
+        //determines the distance in meters that the vessel would travel at
+        // the given speed(in knots) for the given time(in minutes)
+    }
+
+
+    private float[] calculateCoordinates(float[] coordinates, float heading){
+        //calculates the destination coordinates given the initial coordinates, heading, and time traveled.
+    }
+
+
+    private float[] setPrimaryBoundry(){
+        //get last known coordinates
+        int distance = getDistance(travelTime,vesselSpeed);
+        float heading = getHeading()
+
+        primaryBoundry = calculateCoordinates(initialCoordinates, getHeading());
+
+        primaryBoundry;
+    }
+
+
+
+   public void setOuterBoundryCoordinates(){
+
+       int currentTime = 0;
+       float[] currentCoordinates = initialCoordinates;
+       float initialHeading = getHeading();
+       float currentHeading = getHeading();
+
+        while(currentTime <= travelTime){
+            outerPoints.add(calculateCoordinates(initialCoordinates, currentHeading))
+            currentTime++;
+           currentHeading+=initialHeading;
+        }
+
+
 }

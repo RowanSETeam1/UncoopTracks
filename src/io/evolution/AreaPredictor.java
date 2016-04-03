b0873357a53ce6267b93de7be2189e21123838dc
Roberto Gonzalez <gonzal99@students.rowan.edu>
2016-04-03 12:16:52 -0400

    added filler classes

--- src/io/evolution/AreaPredictor.java
+++ src/io/evolution/AreaPredictor.java
@@ -1,7 +1,15 @@
 package io.evolution;
 
+import java.sql.Connection;
+
 /**
  * Created by 1994r on 4/3/2016.
  */
 public class AreaPredictor {
+    public AreaPredictor(Connection c, String MMSI, String startDate, String startTime, String endDate, String endTime){
+
+    }
+    public boolean execute(){
+        return true;
+    }
 }

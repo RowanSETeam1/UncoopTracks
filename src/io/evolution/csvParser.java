7cf3ce9f0e07df3e2ad259f270f846d14ddec61d
Roberto Gonzalez <gonzal99@students.rowan.edu>
2016-04-03 11:28:47 -0400

    modify csvParser

--- src/io/evolution/csvParser.java
+++ src/io/evolution/csvParser.java
@@ -2,20 +2,22 @@ package io.evolution;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
-
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import static io.evolution.Constants.*;
 
 /**
  * Created by gonzal99 on 3/23/2016.
  */
 public class csvParser {
+    //CONSTANTS
+
     Iterable<CSVRecord> csvRecordIterable;
     File csvFile;
     Connection c;
@@ -24,80 +26,74 @@ public class csvParser {
         Reader csvReader = new FileReader(csvFile);
         this.csvFile = csvFile;
         this.c = c;
-        csvRecordIterable = CSVFormat.EXCEL.withHeader("DATETIME",
-                "MMSI",
-                "LATITUDE",
-                "LONGITUDE",
-                "COURSE",
-                "SPEED",
-                "HEADING",
-                "IMO",
-                "NAME",
-                "CALLSIGN",
-                "AISTYPE",
-                "A",
-                "B",
-                "C",
-                "D",
-                "DRAUGHT",
-                "DESTINATION",
-                "ETA").parse(csvReader);
+        //define csv headers
+        csvRecordIterable = CSVFormat.EXCEL.withHeader(Constants.DATETIME,
+                MMSI,
+                LAT,
+                LONG,
+                COURSE,
+                SPEED,
+                HEADING,
+                IMO,
+                NAME,
+                CALLSIGN,
+                AISTYPE,
+                A,
+                B,
+                C,
+                D,
+                DRAUGHT,
+                DESTINATION,
+                ETA).parse(csvReader);
     }
 
     public void iterateCsv() throws SQLException, InterruptedException {
         int i = 0;
         for (CSVRecord record : csvRecordIterable) {
             if (i > 0) {
-                StringBuilder queryBuilder = new StringBuilder("INSERT INTO aisData VALUES (00,");
-                queryBuilder.append("'" + record.get("DATETIME") + "'");
+                StringBuilder queryBuilder = new StringBuilder("INSERT INTO"+tableName+"VALUES (00,");
+                queryBuilder.append("'" + record.get(DATETIME) + "'");
                 queryBuilder.append(",");
-                queryBuilder.append("'" + record.get("MMSI") + "'");
+                queryBuilder.append("'" + record.get(MMSI) + "'");
                 queryBuilder.append(",");
-                queryBuilder.append(Float.parseFloat(record.get("LATITUDE")));
+                queryBuilder.append(Float.parseFloat(record.get(LAT)));
                 queryBuilder.append(",");
-                queryBuilder.append(Float.parseFloat(record.get("LONGITUDE")));
+                queryBuilder.append(Float.parseFloat(record.get(LONG)));
                 queryBuilder.append(",");
-                queryBuilder.append(Float.parseFloat(record.get("COURSE")));
+                queryBuilder.append(Float.parseFloat(record.get(COURSE)));
                 queryBuilder.append(",");
-                queryBuilder.append(Float.parseFloat(record.get("SPEED")));
+                queryBuilder.append(Float.parseFloat(record.get(SPEED)));
                 queryBuilder.append(",");
-                queryBuilder.append(Integer.parseInt(record.get("HEADING")));
+                queryBuilder.append(Integer.parseInt(record.get(HEADING)));
                 queryBuilder.append(",");
-                queryBuilder.append("'" + record.get("IMO") + "'");
+                queryBuilder.append("'" + record.get(IMO) + "'");
                 queryBuilder.append(",");
-                queryBuilder.append("'" + record.get("NAME") + "'");
+                queryBuilder.append("'" + record.get(NAME) + "'");
                 queryBuilder.append(",");
-                queryBuilder.append("'" + record.get("CALLSIGN") + "'");
+                queryBuilder.append("'" + record.get(CALLSIGN) + "'");
                 queryBuilder.append(",");
-                queryBuilder.append("'" + record.get("AISTYPE") + "'");
+                queryBuilder.append("'" + record.get(AISTYPE) + "'");
                 queryBuilder.append(",");
-                queryBuilder.append(Integer.parseInt(record.get("A")));
+                queryBuilder.append(Integer.parseInt(record.get(A)));
                 queryBuilder.append(",");
-                queryBuilder.append(Integer.parseInt(record.get("B")));
+                queryBuilder.append(Integer.parseInt(record.get(B)));
                 queryBuilder.append(",");
-                queryBuilder.append(Integer.parseInt(record.get("C")));
+                queryBuilder.append(Integer.parseInt(record.get(C)));
                 queryBuilder.append(",");
-                queryBuilder.append(Integer.parseInt(record.get("D")));
+                queryBuilder.append(Integer.parseInt(record.get(D)));
                 queryBuilder.append(",");
-                queryBuilder.append(Float.parseFloat(record.get("DRAUGHT")));
+                queryBuilder.append(Float.parseFloat(record.get(DRAUGHT)));
                 queryBuilder.append(",");
-                queryBuilder.append("'" + record.get("DESTINATION") + "'");
+                queryBuilder.append("'" + record.get(DESTINATION) + "'");
                 queryBuilder.append(",");
-                queryBuilder.append("'" + record.get("ETA") + "'");
+                queryBuilder.append("'" + record.get(ETA) + "'");
                 queryBuilder.append(")");
                 PreparedStatement insertData = c.prepareStatement(queryBuilder.toString());
                 insertData.execute();
-
-
             }
             i++;
         }
-        PreparedStatement get = c.prepareStatement("SELECT * FROM aisData;");
-        ResultSet resultSet = get.executeQuery();
-        while (resultSet.next()) {
-            System.out.println(resultSet.getString("mmsi"));
-            System.out.println("\n");
-        }
+
 
     }
 

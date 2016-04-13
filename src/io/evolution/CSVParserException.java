050e6c27c2d6a95e06b8de51897d64bb6f303ae3
Roberto Gonzalez <gonzal99@students.rowan.edu>
2016-04-13 19:20:02 -0400

    custom exception

---
+++ src/io/evolution/CSVParserException.java
@@ -0,0 +1,14 @@
+package io.evolution;
+
+/**
+ * The type Csv parser exception.
+ */
+public class CSVParserException extends Exception{
+    /**
+     * Instantiates a new Csv parser exception.
+     *
+     */
+    public CSVParserException() {
+        super("There was a fatal error");
+    }
+}

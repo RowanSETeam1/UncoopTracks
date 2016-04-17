39cc435391dbcc5bfbfc3a7fc75f91a5e78f19f2
eliakah <kakoue66@students.rowan.edu>
2016-04-17 15:25:09 -0400

    original iml

--- src/io/evolution/KmlGenerator.java
+++ src/io/evolution/KmlGenerator.java
@@ -137,11 +137,14 @@ filename, "UTF-8");
         tag +=  origin.getLongitude() + "," + origin.getLatitude()+"\n";
         for (int i = 2; i < size; i++) {
             tag +=  points.get(i).getLongitude() + "," + points.get(i).getLatitude()+"\n";
+            System.out.println(points.get(i).getLongitude() + "," + points.get(i).getLatitude()+"\n");
         }
         tag +=  second_p.getLongitude() + "," + second_p.getLatitude()+"\n";
+        System.out.println(second_p.getLongitude() + "," + second_p.getLatitude()+"\n");
 
         for (int i = (points.size()-1); i >= size ; i--) {
             tag +=  points.get(i).getLongitude() + "," + points.get(i).getLatitude()+"\n";
+            System.out.println(points.get(i).getLongitude() + "," + points.get(i).getLatitude()+"\n");
         }
 
         tag += "</coordinates>\n" +
